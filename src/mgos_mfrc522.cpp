bf7391bc79a7cd1e0e59b1921a7e02fb04b05b4d
Peter <branco@wp.pl>
2019-02-24 11:41:24 +0100

    pin bug cpp

--- src/mgos_mfrc522.cpp
+++ src/mgos_mfrc522.cpp
@@ -1,11 +1,11 @@
 #include "mgos_mfrc522.h"
 
 MFRC522 *mgos_mfrc522_create() {
-  return new MFRC522(SS_PIN,RST_PIN);
+  return new MFRC522();
 }
 
 void mgos_mfrc522_init(MFRC522 *rfid)
  {
   if (rfid == nullptr) return;
-  rfid->PCD_Init();
+  rfid->PCD_Init(SS_PIN,RST_PIN);
 }
