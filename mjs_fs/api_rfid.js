97ae215547fd53e1b1b1073c3aa6acc2d2942c38
Peter <branco@wp.pl>
2019-02-24 21:21:04 +0100

    fix pins

--- mjs_fs/api_rfid.js
+++ mjs_fs/api_rfid.js
@@ -2,7 +2,7 @@ let MFRC522 = {
     _create: ffi('void *mgos_mfrc522_create()'),
     _init: ffi('void *mgos_mfrc522_init(void *)'),
 
-    create: function(rst, res) {
+    create: function() {
         let obj = Object.create(MFRC522._proto);
         obj.rfid = MFRC522._create(21,22);
         return obj;
