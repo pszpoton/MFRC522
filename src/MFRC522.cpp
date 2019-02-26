2eb987ac2d2e2ff152c01d8d7ade6b65732e481b
Peter <branco@wp.pl>
2019-02-26 18:23:41 +0100

    Update MFRC522.cpp

--- src/MFRC522.cpp
+++ src/MFRC522.cpp
@@ -1781,11 +1781,13 @@ bool MFRC522::PICC_ReadCardSerial()
 char* MFRC522::PICC_getUID()
 {
     // return uid.uidByte[0];
+    
     char str[128];
     str[0] = 8;
     int i=0;
     int index = 0;
     for (i=0; i<10; i++){
+        printf("%d\n", uid.uidByte[i]);
         index += sprintf(&str[index], "%d", uid.uidByte[i]);
     }
     return str;
